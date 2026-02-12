ef979fb66a3907f8a7996caee358f5ce301adb9c
Ubuntu <ubuntu@ip-172-31-8-235.ec2.internal>
2026-02-12 15:10:33 +0000

    added all file day02

---
+++ shell-scripting/day-02/error-handle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo " finding error-in deployment project"
+
+create_directory(){
+	mkdir demo
+}
+
+if ! create_directory; then
+	echo " folder already exist found error to create"
+	exit 1
+fi
+
+
+
+echo "code is interupted"
+
+
+
+
+
